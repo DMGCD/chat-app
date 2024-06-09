b982157839f97a5a4f7a7f6f9ba0d19eb60cd377
Thushara Abeykoon <gcdisanayaka1998@gmail.com>
2024-06-09 23:50:36 +0530

    removed unwanted stuff

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,49 +13,6 @@ function App() {
   const [isLogged, setIsLogged] = useState(false);
   const [username, setUsername] = useState("");
 
-  // const register = () => {
-  //   const username = usernameRef.current.value;
-  //   if (username !== ''){
-  //     socket.emit("registerUser", {username});
-  //     setIsRegistered(true);
-  //   }
-  // }
-
-  // const sendMessage = () => {
-  //   const receiverUsername = receiverRef.current.value;
-  //   const msgContent = messageRef.current.value;
-  //   if (receiverUsername!=='' && msgContent!=='')
-  //     socket.emit("privateMsg", {receiverUsername:receiverRef.current.value, msgContent:messageRef.current.value});
-  // }
-
-  // const sendGroupMessage = () => {
-  //   const roomName = roomNameRef.current.value;
-  //   const content = groupMessageRef.current.value;
-  //   if(roomName!=='' && content !== '')
-  //     socket.emit("groupMessage",{roomName, content});
-  // }
-
-  // const joinRoom = () => {
-  //   const roomName = roomNameRef.current.value;
-  //   if(roomName!=='')
-  //     socket.emit('joinRoom',roomName);
-  // }
-
-  useEffect(()=>{
-    socket.on("receivePrivateMsg", (data)=>{
-      console.log("private message",data);
-    })
-
-    socket.on("receiveGroupMessage", (data)=>{
-      console.log(data);
-    })
-
-    socket.on("error",data => {
-      console.log(data);
-    })
-
-  },[socket]);
-
   return (
     <AppContext.Provider value={{ socket, setIsLogged, username, setUsername }}>
       {!isLogged? <Login />:<Dashboard />}
