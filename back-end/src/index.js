e6b26545fffb8cf5613fc4981b26c4f904342f94
Thushara Abeykoon <gcdisanayaka1998@gmail.com>
2024-06-09 17:20:35 +0530

    added private message sending capability

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import User from './model/User.js';
 import userRouter from './routes/userRoutes.js';
+import Message from './model/Message.js';
+import Chat from './model/Chat.js';
 
 dotenv.config();
 
@@ -43,11 +45,49 @@ io.on("connection", (socket)=>{
         console.log("user has been saved");
     });
 
+    socket.on("privateMsg", async ({receiverUsername, msgContent}) => {
+        // suppose that every sender has been registered
+        const sender = await User.findOne({socketId: socket.id});
+        const receiver = await User.findOne({username:receiverUsername});
+
+        if (!receiver)
+            socket.to(socket.id).emit("error",{error: `User with ${receiverUsername} isn't found!`});
+        else {
+            // Access private chat belong to sender and receiver
+            const getChat = async() => {
+                const chatName1 = `${sender.username}&${receiver.username}`;
+                const chatName2 = `${receiver.username}&${sender.username}`;
+    
+                let chat = await Chat.findOne({ chatName: chatName1 }) || await Chat.findOne({ chatName: chatName2 });
+                
+                if (!chat){
+                    chat = new Chat({
+                        chatName:`${sender.username}&${receiver.username}`,
+                        isGroupChat:false,
+                        users: [sender._id, receiver._id],
+                    })
+                    chat.save();
+                }
+                return chat;
+            }
+
+            const chat = getChat();
+
+            new Message({
+                sender: sender._id,
+                content: msgContent,
+                chat: chat._id
+            }).save();
+
+            socket.to(receiver.socketId).emit("receivePrivateMsg",msgContent);
+        }
+    })
+
     socket.on("disconnect", ()=>{
         console.log(`User:${socket.id} has been disconnected!`);
     })
-
 })
+
 
 server.listen(PORT, ()=> {
     console.log(`SERVER IS RUNNING ON ${PORT}`);
--- back-end/src/model/Chat.js
+++ back-end/src/model/Chat.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const ChatModel = mongoose.Schema({
-    chatName: { type:String, trim:true },
+    chatName: { type:String, trim:true, unique: true },
     isGroupChat: { type: Boolean, default: false },
     users: [
         { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     ],
-    latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" }
+    // latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" }
 });
 
 const Chat = mongoose.model("Chat", ChatModel);
