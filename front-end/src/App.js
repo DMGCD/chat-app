ca39fb1e699d2936bb1d23575447b4c42e8716fb
Thushara Abeykoon <gcdisanayaka1998@gmail.com>
2024-06-09 18:57:24 +0530

    Created Login logout functionality

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,63 @@
+import { createContext, useEffect, useRef, useState } from 'react';
 import './App.css';
 import { io } from 'socket.io-client';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 const socket = io.connect("http://localhost:8080");
 
+export const AppContext = createContext();
+
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  // const register = () => {
+  //   const username = usernameRef.current.value;
+  //   if (username !== ''){
+  //     socket.emit("registerUser", {username});
+  //     setIsRegistered(true);
+  //   }
+  // }
+
+  // const sendMessage = () => {
+  //   const receiverUsername = receiverRef.current.value;
+  //   const msgContent = messageRef.current.value;
+  //   if (receiverUsername!=='' && msgContent!=='')
+  //     socket.emit("privateMsg", {receiverUsername:receiverRef.current.value, msgContent:messageRef.current.value});
+  // }
+
+  // const sendGroupMessage = () => {
+  //   const roomName = roomNameRef.current.value;
+  //   const content = groupMessageRef.current.value;
+  //   if(roomName!=='' && content !== '')
+  //     socket.emit("groupMessage",{roomName, content});
+  // }
+
+  // const joinRoom = () => {
+  //   const roomName = roomNameRef.current.value;
+  //   if(roomName!=='')
+  //     socket.emit('joinRoom',roomName);
+  // }
+
+  // useEffect(()=>{
+  //   socket.on("receivePrivateMsg", (data)=>{
+  //     console.log("private message",data);
+  //   })
+
+  //   socket.on("receiveGroupMessage", (data)=>{
+  //     console.log(data);
+  //   })
+
+  //   socket.on("error",data => {
+  //     console.log(data);
+  //   })
+
+  // },[socket]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <AppContext.Provider value={{ socket, setIsLogged }}>
+      {!isLogged? <Login />:<Dashboard />}
+    </AppContext.Provider>
   );
 }
 
---
+++ front-end/src/components/Login.js
@@ -0,0 +1,26 @@
+import React, { useContext, useRef } from 'react'
+import { AppContext } from '../App';
+
+const Login = () => {
+
+    const { socket, setIsLogged } = useContext(AppContext);
+
+    const usernameRef = useRef();
+
+    const login = () => {
+        const username = usernameRef.current.value;
+        if (username !== ""){
+            socket.emit("registerUser", { username });
+            setIsLogged(true);
+        }
+    }
+
+  return (
+    <div>
+        <input ref={usernameRef} type='text' placeholder='Username' autoFocus />
+        <button onClick={login}>Login</button>
+    </div>
+  )
+}
+
+export default Login
