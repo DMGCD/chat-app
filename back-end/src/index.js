0cf03fe37a53701258ded6a326d25b5afe7dc6b8
Thushara Abeykoon <gcdisanayaka1998@gmail.com>
2024-06-09 13:35:59 +0530

    added user registration

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { Server } from 'socket.io';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import User from './model/User.js';
 
 dotenv.config();
 
@@ -25,7 +26,16 @@ const io = new Server(server, {
 io.on("connection", (socket)=>{
     console.log(`User:${socket.id} has been connected!`);
 
-    
+    socket.on("registerUser", async(data)=>{
+        const username = data.username;
+        let user = await User.findOne({username});
+        if (!user)
+            user = new User({username, socketId: socket.id});
+        else
+            user.socketId = socket.id;
+        await user.save();
+        console.log("user has been saved");
+    })
 
     socket.on("disconnect", ()=>{
         console.log(`User:${socket.id} has been disconnected!`);
